5f5f1acedd11465b9af91a12e953ec81c6fc50d9
Nikola Bebic <bn140314d@student.etf.bg.ac.rs>
2017-08-17 20:10:16 +0200

    Operand Type enum updated in assembler

--- VisualStudioProjects/Assembler/Assembler/OperandType.h
+++ VisualStudioProjects/Assembler/Assembler/OperandType.h
@@ -1,20 +1,21 @@
 #ifndef _operand_type_h_
 #define _operand_type_h_
+#include <cstdint>
 
 namespace bnssassembler {
 	
 	/**
 	 * \brief Enum representing the operand type
 	 */
-	enum OperandType {
-		DEFAULT = 0,
-		UNSIGNED_BYTE = 1,
-		SIGNED_BYTE = 2,
-		REGULAR_BYTE = 3,
-		UNSIGNED_WORD = 4,
-		SIGNED_WORD = 5,
-		REGULAR_WORD = 6,
-		REGULAR_DOUBLE_WORD = 7
+	enum OperandType : int8_t {
+		DEFAULT					= 0b000,
+		UNSIGNED_BYTE			= 0b011,
+		SIGNED_BYTE				= 0b111,
+		REGULAR_BYTE			= 0b111,
+		UNSIGNED_WORD			= 0b001,
+		SIGNED_WORD				= 0b101,
+		REGULAR_WORD			= 0b101,
+		REGULAR_DOUBLE_WORD		= 0b000
 	};
 }
 
