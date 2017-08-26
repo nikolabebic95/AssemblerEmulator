4ac3140fcfa803864b454752ac317dff0c80e54d
Nikola Bebic <bn140314d@student.etf.bg.ac.rs>
2017-08-26 02:28:37 +0200

    Modulo fix

--- VisualStudioProjects/Emulator/Emulator/ModuloExecuter.cpp
+++ VisualStudioProjects/Emulator/Emulator/ModuloExecuter.cpp
@@ -1,8 +1,13 @@
 #include "ModuloExecuter.h"
+#include "MessageException.h"
 
 namespace bnssemulator {
 
 	void ModuloExecuter::execute(Register & dst, const Register & lhs, const Register & rhs) const {
+		if (rhs == 0) {
+			throw MessageException("Modulo by zero");
+		}
+
 		dst = lhs % rhs;
 	}
 }
